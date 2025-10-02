e9f65863eb5f0f8832f64838655cdd8659476850
wangyibo <430347019@qq.com>
2025-10-02 10:03:51 +0800

    feat: PWM

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,9 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
+#include <math.h>
+
 #include "tim.h"
 #include "gpio.h"
 
@@ -95,20 +98,26 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start(&htim1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    uint32_t arr_value = __HAL_TIM_GetCounter(&htim1)+1;
+    uint32_t brightness = arr_value * sinf(4*HAL_GetTick()/1000.f) - 1 ;
+    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, brightness);
+    /*
     if (__HAL_TIM_GET_COUNTER(&htim1) > __HAL_TIM_GET_AUTORELOAD(&htim1)/2)
     {
       HAL_GPIO_WritePin(LEDG_GPIO_Port, LEDG_Pin, GPIO_PIN_RESET);
     } else
     {
+
       HAL_GPIO_WritePin(LEDG_GPIO_Port, LEDG_Pin, GPIO_PIN_SET);
     }
+    */
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
