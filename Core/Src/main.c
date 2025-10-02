29f31184fca4e377015687cc358f79a128655ca0
wangyibo <430347019@qq.com>
2025-10-02 15:37:52 +0800

    feat: 串口发送

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "tim.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -94,15 +95,19 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM1_Init();
+  MX_UART7_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start_IT(&htim1);
-  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  uint8_t tx_msg[] = "RoboMaster";
+  //HAL_TIM_Base_Start_IT(&htim1);
+  //HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    HAL_UART_Transmit(&huart7, tx_msg, 10, 1000);
+    HAL_Delay(1000);
     // uint32_t arr_value = __HAL_TIM_GetCounter(&htim1)+1;
     // uint32_t brightness = arr_value * sinf(4*HAL_GetTick()/1000.f) - 1 ;
     // __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, brightness);
